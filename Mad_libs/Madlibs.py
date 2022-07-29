7e0ba2d107fdbaf3999d052af3cfa48581eea208
kazmiware <kazmi1744@gmail.com>
2022-07-29 16:00:44 +0500

    Added Madlibs project

---
+++ Mad_libs/Madlibs.py
@@ -0,0 +1,6 @@
+name=input("Enter your name:")
+profession=input("Enter your favourite profession:")
+noun=input("Enter any noun:")
+print(f"My name is {name}, i am a great {profession}")
+print(f"My target is to become the greatest {profession}")
+print(f"During my journey i want to have many {noun}")

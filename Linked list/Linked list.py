ac9a0ffd0b4acf6104686a17be30cf6c324500b2
kazmiware <kazmi1744@gmail.com>
2022-11-17 19:07:24 +0500

    added linked list

--- Linked list/Linked list.py
+++ Linked list/Linked list.py
@@ -1,54 +1,63 @@
 class linked_list:
+    def __init__(self,last_node=None,head=None,node=None):
+        self.head=head
+        self.node=node
+        self.last_node=last_node
 
-    linked_lst={}
+    def add_item(self,item):
+        if self.head==None:
+            self.head=Node(item)
+        elif self.node==None:
+            self.node=Node(item)
+            self.head.next=self.node
+            self.last_node=self.node
+        else:
+            self.last_node.next=Node(item)
+            self.last_node=self.last_node.next
 
-    def __init__(self,ele=[],root=None,next=None):
-        self.ele = ele
-        self.root = root
-        self.next = next
 
-    def create_list(self):
-        count = 0
-        for i in self.ele:
-            self.linked_lst[i] = count+1
+    def print_list(self):
+        itr=self.head
+        while True:
+            if itr==None:
+                return
+            print(itr.val,end='')
+            if itr.next!=None:
+                print("-->",end='')
+            itr=itr.next
+
+    def add_at(self,item,index):
+        count=0
+        element=self.head
+        while True:
+            if count==index-1:
+                item=Node(item)
+                item.next=element.next
+                element.next=item
+                break
+            element=element.next
             count+=1
 
-    def add_start(self,node):
-        for i ,j in self.linked_lst.items():
-           self.linked_lst[i]= j+1
-        self.linked_lst[node] = 1
-
-    def add_middile(self,node,index):
-        for i,j in self.linked_lst.items():
-            if j>index:
-                self.linked_lst[i] = j+1
-        self.linked_lst[node] = index+1
-
-    def add_end(self,node):
-        self.linked_lst[node] = len(self.linked_lst)+1
+    def add_beginning(self,item):
+        item=Node(item)
+        item.next=self.head
+        self.head=item
+        return
 
 
-    def print_ll(self):
-        count = 1
-        while True:
-            if count not in range(len(self.linked_lst.keys()) + 1):
-                break
-            for i ,j in self.linked_lst.items():
-                if j == count:
-                    print(i,end='')
-                    if count!=len(self.linked_lst):
-                        print('--->',end='')
-                    count+=1
+class Node:
+    def __init__(self,val=None,next=None):
+        self.val=val
+        self.next=next
 
 
 
-
-l_l=linked_list([10,9,3,15])
-l_l.create_list()
-l_l.add_start(1)
-l_l.add_middile(11,4)
-l_l.add_end(20)
-l_l.print_ll()
-
-
-
+ll=linked_list()
+ll.add_item(3)
+ll.add_item(7)
+ll.add_item(9)
+ll.add_item(5)
+ll.add_at(6,1)
+ll.add_at(10,4)
+ll.add_beginning(0)
+ll.print_list()
